d9bfa75b35385c2166c0df9d76303c3e081155cd
Henrique <nuneshp2@gmail.com>
2022-11-07 20:28:29 -0300

    components dinamycs propertys

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: [
+    './small-card.component.css',
+    './small-card.responsive.component.css']
 })
 export class SmallCardComponent implements OnInit {
+  
+  @Input()
+  photoCover:string = "https://midias.correiobraziliense.com.br/_midias/jpg/2022/06/24/hotd_ka_hbo_max_organic_1x1_bra__2_-25934247.jpg"
+  
+  @Input()
+  cardTitle:string = "Segunda temporada de A Casa do Dragão"
 
   constructor() { }
 
